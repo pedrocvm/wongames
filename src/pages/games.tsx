a0e8538556fbb1f0ad548a29711032251f5d9a67
pedrocvm <pedrocvm2@gmail.com>
2023-01-10 16:47:06 +0000

    install codegen and types generator

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,15 +1,36 @@
 import Games, { GamesProps } from 'templates/Games';
 import filtersMock from 'components/ExploreSidebar/mock';
-import gamesMock from 'components/GameCardSlider/mock';
+import { initializeApollo } from 'utils/apollo';
+import { QUERY_GAMES } from 'graphql/queries/games';
 
 export default function GamesPage(props: GamesProps) {
   return <Games {...props} />;
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo();
+
+  const { data } = await apolloClient.query({
+    query: QUERY_GAMES,
+    variables: {
+      limit: 9
+    }
+  });
+
   return {
     props: {
-      games: gamesMock,
+      revalidate: 60,
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: game.cover && game.cover.url,
+        price: game.price
+          ? new Intl.NumberFormat('en', {
+              style: 'currency',
+              currency: 'USD'
+            }).format(game.price)
+          : 'FREE TO PLAY'
+      })),
       filterItems: filtersMock
     }
   };
